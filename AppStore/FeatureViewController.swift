73602282c0498d62fa4c2b28cfa3c338f58d2bf3
ThangLQ <thanglq@ThangLQs-iMac.local>
2017-04-11 00:39:45 -0700

    First commit

---
+++ AppStore/FeatureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by ThangLQ on 4/11/17.
+//  Copyright © 2017 ThangLQ. All rights reserved.
+//
+
+import UIKit
+
+class FeatureViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.white
+    }
+}
+
